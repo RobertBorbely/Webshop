9a0fea6a4d179ddb7a4a600d396519401e4eb9d6
Borbély Róbert <respectsas@gmail.com>
2016-02-01 16:24:54 +0100

    Added production mode

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,5 +2,7 @@ class Attachment < ActiveRecord::Base
   belongs_to :products, polymorphic: true
 
   has_attached_file :image, styles: { tumbnail: "150x150", medium: "600x600", large: "1920x1080"}
+    :url => "/system/:attachment/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
 	has_many :products, :dependent => :destroy
-  has_attached_file :image, styles: { tumbnail: "150x150", medium: "600x600", large: "1920x1080"}
+  has_attached_file :image, styles: { tumbnail: "150x150", medium: "600x600", large: "1920x1080"},
+    :url => "/system/:attachment/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	def should_generate_new_friendly_id?
