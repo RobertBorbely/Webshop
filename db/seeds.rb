8e34e10e52b3c85a0bf51ea45404ccdccd12d840
Borbély Róbert <respectsas@gmail.com>
2016-05-16 14:27:08 +0200

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
+Product.create!(name: "First item", description: "Whoo add more me!", price: 10, count: 10)
+Review.create!(product_id: 1, rating: 5, user_id: 1, text: 'First review')
